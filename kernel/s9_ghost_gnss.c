f73629b4c64ad2e2308532c273e47a131ac3ac51
khiconjk <khiconjk@users.noreply.github.com>
2026-09-21 19:16:17 +0700

    fix: -Wdeclaration-after-statement in s9_ghost_gnss.c

--- kernel/s9_ghost_gnss.c
+++ kernel/s9_ghost_gnss.c
@@ -545,15 +545,26 @@ static void s9_ghost_gnss_worker(struct work_struct *work)
 
 		/* $GPRMC - Recommended Minimum sentence (W3) */
 		{
-			u64 day = (sec / 86400);
-			u64 y400 = day / 146097; day %= 146097;
-			u64 y100 = day / 36524; if (y100 == 4) y100 = 3; day -= y100 * 36524;
-			u64 y4 = day / 1461; day %= 1461;
-			u64 y1 = day / 365; if (y1 == 4) y1 = 3; day -= y1 * 365;
-			u64 year = 1970 + y400 * 400 + y100 * 100 + y4 * 4 + y1;
-			int month, mday;
 			static const int mdays[] = {31,28,31,30,31,30,31,31,30,31,30,31};
-			bool leap = (year%4==0 && (year%100!=0 || year%400==0));
+			u64 day, y400, y100, y4, y1, year;
+			int month, mday;
+			bool leap;
+
+			day = (sec / 86400);
+			y400 = day / 146097;
+			day %= 146097;
+			y100 = day / 36524;
+			if (y100 == 4)
+				y100 = 3;
+			day -= y100 * 36524;
+			y4 = day / 1461;
+			day %= 1461;
+			y1 = day / 365;
+			if (y1 == 4)
+				y1 = 3;
+			day -= y1 * 365;
+			year = 1970 + y400 * 400 + y100 * 100 + y4 * 4 + y1;
+			leap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 
 			for (month = 0; month < 12; month++) {
 				int md = mdays[month] + (month == 1 && leap ? 1 : 0);
